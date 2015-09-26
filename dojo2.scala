b24a8dc54b32700aff7370d2ea8fcfcfe25fa729
6Mario6 <mario66fernandez@gmail.com>
2015-09-26 12:16:50 -0500

    first commit

---
+++ dojo2.scala
@@ -0,0 +1,11 @@
+object dojo2{
+ def main(args: Array[String]) {
+    val lista= List(1,2,3)
+    lista.map((x:Int)=> println(x+4))
+println(suma2(2,2))
+}
+def suma(x:Int,y:Int)=x+y
+val suma2=(x:Int,y:Int)=> x+y
+//  def mayor(x:Int,y:Int){if}
+
+}
